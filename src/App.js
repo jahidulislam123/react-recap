5d5fa5a4ff680c66c5b2303f8f108914d178ad6c
unknown <islam15-3879@diu.edu.bd>
2022-03-20 01:02:08 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Districts name="jahidul islam" special="vibag"  ></Districts>
+      <Districts name="mahidul islam" special="maramari"  ></Districts>
+      <Districts name="rakibul islam" special="santovai "  ></Districts>
+      <Districts name="ramima islam" special="moynamoti "  ></Districts>
+      
+      
     </div>
   );
+}
+
+const districstyle={
+  backgroundColor:'lightblue',
+  padding:'23px',
+  margin:'20px',
+  borderRadius:'20px'
+
+}
+function Districts(props){
+  const[power,setPower]=useState(1);
+  const boostPower=()=>{
+    const newPower=power*2;
+    setPower(newPower);
+
+  }
+
+  return(
+
+    <div style={districstyle} >
+      <h2>Name :{props.name} </h2>
+      <p>Speciality :{props.special}</p>
+      <h4>Power :{power}</h4>
+      <button onClick={boostPower} >Boost the power</button>
+
+    </div>
+  )
 }
 
 export default App;
